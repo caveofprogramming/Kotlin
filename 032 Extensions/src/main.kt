81dd9630f19046c57edaaff238b8f1a58e47f663
John <john@Johns-iMac.station>
2024-12-23 15:33:32 +0100

    Updated.

---
+++ 032 Extensions/src/main.kt
@@ -0,0 +1,28 @@
+class Machine(val name: String) {
+    fun start() {
+        println("$name starting.")
+    }
+}
+
+fun Machine.printInfo() {
+    println("Identifier: ${this.name}, Version: $version")
+}
+
+val Machine.version:Int
+    get() {
+        return 1
+    }
+
+fun main() {
+    val m1: Machine = Machine("T1000")
+    m1.start()
+
+    m1.printInfo()
+
+    fun Int.greet() {
+        println("Hello")
+    }
+
+    val number:Int = 7
+    7.greet()
+}
